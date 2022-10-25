4f067535542ca97d94b9fe5ada15a43655bdb515
Tsui, Dennis Hoyin <dtsui9@gatech.edu>
2022-10-24 20:00:04 -0400

    Update and rename test.py to monkeylearn_keyword_extraction.py

--- keyword_search/test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from sqlalchemy import create_engine
-import py_mysql_connector
-
-engine = create_engine("mysql+mysqlconnector://root:password@localhost:3306/its")
-query = pd.read_sql_query('SELECT content FROM eSPFirst WHERE chapter = 1', engine)
-df = pd.DataFrame(query)
-print(df["content"].tolist())
