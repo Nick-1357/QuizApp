5a083945b47c704f61ef5ea8d4409f1399a158f2
Memesh, Abdulaziz Ahmed <a.memesh@gatech.edu>
2022-12-15 23:06:27 -0500

    Apply Varun's implementation for user email field

--- QuizApp/app/src/main/java/edu/gatech/seclass/quizapp/CreateAccount.java
+++ QuizApp/app/src/main/java/edu/gatech/seclass/quizapp/CreateAccount.java
@@ -38,6 +38,7 @@ public class CreateAccount extends AppCompatActivity  {
     private EditText name;
     private EditText password;
     private EditText username;
+    private EditText email;
     private ImageView pfp;
     private static final int RQS_OPEN_IMAGE = 1;
 
@@ -52,6 +53,7 @@ public class CreateAccount extends AppCompatActivity  {
         username = (EditText) findViewById(R.id.create_account_username);
         password = (EditText) findViewById(R.id.create_account_password);
         name = (EditText)  findViewById(R.id.create_account_name);
+        email = (EditText) findViewById(R.id.create_account_email);
         pfp = (ImageView) findViewById(R.id.profile_picture);
         browse = (Button) findViewById(R.id.create_account_browse);
 
@@ -99,6 +101,26 @@ public class CreateAccount extends AppCompatActivity  {
     }
 
     public void createAccount (View view) {
+        //send user confirmation email
+        String[] TO = {email.getText().toString()};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+
+
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Quiz App Registration");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Email confirmation for Quiz App account creation and credentials are: " + "name is " + name.getText().toString() + " username is " + username.getText().toString() + " password is " + password.getText().toString());
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+            finish();
+            Toast.makeText(CreateAccount.this,"Email sent", Toast.LENGTH_SHORT).show();
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(CreateAccount.this,
+                    "There is no email client installed.", Toast.LENGTH_SHORT).show();
+        }
+
         // check if userId, name, password are blank
         boolean usernameBlank = username.getText().toString().length()==0;
         boolean nameBlank = name.getText().toString().length()==0;
